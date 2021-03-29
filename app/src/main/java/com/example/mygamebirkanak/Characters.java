ba27a79f18f0b17e11d8648b86ff9196db5ddf68
BirkanAk <bak1@binghamton.edu>
2021-03-29 04:01:46 +0300

    Characters Class done, added MaterialList

---
+++ app/src/main/java/com/example/mygamebirkanak/Characters.java
@@ -0,0 +1,51 @@
+package com.example.mygamebirkanak;
+
+public class Characters {
+    private String name,description;
+    private int strength,intelligence,agility,crafting,charisma;
+    private int image;
+
+    public Characters(String name, String description, int strength,int intelligence,int agility,int crafting,int charisma,int image) {
+        this.name = name;
+        this.description = description;
+        this.strength=strength;
+        this.intelligence=intelligence;
+        this.agility=agility;
+        this.crafting=crafting;
+        this.charisma=charisma;
+        this.image = image;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public int getCrafting() {
+        return crafting;
+    }
+
+    public int getCharisma() {
+        return charisma;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
--- app/src/main/java/com/example/mygamebirkanak/MainActivity.java
+++ app/src/main/java/com/example/mygamebirkanak/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.mygamebirkanak;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+import com.dexafree.materialList.card.Card;
+import com.dexafree.materialList.card.CardProvider;
+import com.dexafree.materialList.card.OnActionClickListener;
+import com.dexafree.materialList.card.action.TextViewAction;
+import com.dexafree.materialList.view.MaterialListView;
+import com.squareup.picasso.RequestCreator;
 
 public class MainActivity extends AppCompatActivity {
-
+    MaterialListView materialListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        materialListView= (MaterialListView) findViewById(R.id.materialListView);
+        this.loadCharacters();
     }
+
+    private void loadCharacters() {
+        for (Characters c: getData()){
+            this.createCharacter(c);
+        }
+    }
+
+    private Characters[] getData() {
+    }
+
+    private void createCharacter(Characters c) {
+    }
+
+
 }
