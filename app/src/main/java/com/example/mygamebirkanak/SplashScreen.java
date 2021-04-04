eb4d010231f4974d5c56bed6282dd1f7c4d28b95
BirkanAk <bak1@binghamton.edu>
2021-04-05 02:12:11 +0300

    Buttons for Splash Screen done. TODO activity transmission.

--- app/src/main/java/com/example/mygamebirkanak/SplashScreen.java
+++ app/src/main/java/com/example/mygamebirkanak/SplashScreen.java
@@ -2,14 +2,33 @@ package com.example.mygamebirkanak;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+    Button playbutton;
+    Button instructionsbutton;
+    TextView highscoresplash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        playbutton=(Button) findViewById(R.id.play_button);
+        instructionsbutton=(Button) findViewById(R.id.instruction_button);
+        highscoresplash=(TextView)findViewById(R.id.highscoresplash);
+
+        highscoresplash.setAllCaps(true);
+        highscoresplash.setTextSize(44);
+        highscoresplash.setTextColor(Color.parseColor("#D2310F"));
+
+        playbutton.setBackgroundColor(Color.parseColor("#529930"));
+        instructionsbutton.setBackgroundColor(Color.parseColor("#1F4775"));
+
+        highscoresplash.setText("HIGHSCORE:");
 
 
     }
